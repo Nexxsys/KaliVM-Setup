4c289265f49537ce510a22282103dff9bf9d9a01
Nexxsys <Nexxsys@users.noreply.github.com>
2024-09-10 20:50:50 -0600

    Update kali-additions.sh

--- kali-additions.sh
+++ kali-additions.sh
@@ -10,7 +10,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 # Install necessary packages
-sudo apt install -y pipx gdb git sublime-text synaptic apt-transport-https xclip terminator cifs-utils byobu exiftool jq ruby-full docker.io docker-compose locate tldr btop thefuck kali-tools-passwords
+sudo apt install -y pipx gdb git sublime-text apt-transport-https xclip terminator cifs-utils byobu exiftool jq ruby-full docker.io docker-compose locate tldr btop thefuck kali-tools-passwords feroxbuster gobuster
 # Ensure pipx path is available
 pipx ensurepath
 
@@ -145,12 +145,10 @@ cp ./githubdownload.py "$build_dir/githubdownload.py"
 # Download github releases
 sudo python3 "$build_dir/githubdownload.py" "jpillora/chisel" "_linux_amd64.gz" "/opt/chisel"
 sudo python3 "$build_dir/githubdownload.py" "jpillora/chisel" "_windows_amd64.gz" "/opt/chisel"
-# Uncomment the line below if you want to download for macOS
-# python3 "$build_dir/githubdownload.py" "jpillora/chisel" "_darwin_amd64" "/opt/chisel" "chisel_osx"
-sudo python3 "$build_dir/githubdownload.py" "carlospolop/PEASS-ng" "linpeas.sh" "/opt/peas"
-sudo python3 "$build_dir/githubdownload.py" "carlospolop/PEASS-ng" "winPEASx64.exe" "/opt/peas"
+sudo python3 "$build_dir/githubdownload.py" "carlospolop/PEASS-ng" "linpeas.sh" "~/wwwtools/peas"
+sudo python3 "$build_dir/githubdownload.py" "carlospolop/PEASS-ng" "winPEASx64.exe" "/wwwtools/peas"
 sudo python3 "$build_dir/githubdownload.py" "WithSecureLabs/chainsaw" "chainsaw_all_" "/opt/"
-sudo python3 "$build_dir/githubdownload.py" "BloodHoundAD/BloodHound" "BloodHound-linux-x64.zip" "/opt/"
+#sudo python3 "$build_dir/githubdownload.py" "BloodHoundAD/BloodHound" "BloodHound-linux-x64.zip" "/opt/"
 
 # Remove temporary build directory
 sudo rm -rf "$build_dir"
