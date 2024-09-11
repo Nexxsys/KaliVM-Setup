46b5285d1829a1a8850ebf9e5715ad69e27453af
Nexxsys <Nexxsys@users.noreply.github.com>
2024-09-10 21:59:04 -0600

    Update finish.sh

--- finish.sh
+++ finish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# Created based on the teachings of John Hammond
+# This will mark your directory as _COMPLETED for
+# any CTF folder you are presently in on you machine
 original_directory=$(pwd)
 
 # echo $original_directory
