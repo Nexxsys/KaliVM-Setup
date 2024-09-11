71a98296e06e7165a25cce7630ac1a6a87dd2d49
Nexxsys <Nexxsys@users.noreply.github.com>
2024-09-10 21:59:40 -0600

    Update removecomments.sh

--- removecomments.sh
+++ removecomments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Removes comments from the top of files like those from exploitdb
 args=("$@")
 # echo ${args[0]}
 sudo sed -i 's/^\#.*$//g' ${args[0]} && sudo sed -i '/^$/d' ${args[0]}
